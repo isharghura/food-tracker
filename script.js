e9c3d1e0d3427e98d82214c5aa6cdce47a2aaceb
isharghura <ishar.ghuras@gmail.com>
2023-07-28 12:01:15 -0400

    added script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+const searchBar = document.getElementById("search-bar")
+const input = document.getElementById("user-input")
+const title = document.getElementById("title")
+
+document.addEventListener('keydown', (event) => {
+    var isEnterKey = event.key;
+    if (isEnterKey === 'Enter' && input.value !== '') {
+        title.innerHTML = input.value;
+        input.value = ''
+    }
+})
