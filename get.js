b439e4b93b5542bfa34cb5acdfac69d732e886f0
isharghura <ishar.ghuras@gmail.com>
2023-07-29 17:38:21 -0400

    added new get file

---
+++ get.js
@@ -0,0 +1,19 @@
+//get
+
+const fetch = require("node-fetch");
+const config = require("./config.json")
+
+const params = {
+    api_key: config.api_key,
+    query: "apple",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 5,
+}
+
+const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(params.dataType)}&pageSize=${encodeURIComponent(params.pagesize)}`
+
+function get_data() {
+    return fetch(api_url).then(response => response.json())
+}
+
+get_data().then(data => console.log(data.foods[0].foodNutrients))
