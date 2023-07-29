e6930e80376ba09b339106db092ee0c6d7a640c6
isharghura <ishar.ghuras@gmail.com>
2023-07-29 15:05:05 -0400

    added function to script.js

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ document.addEventListener('keydown', (event) => {
         input.value = ''
     }
 })
+
+function getNutritionFacts() {
+    fetch('/get_nutrition', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `food_item=${encodeURIComponent(input)}`
+    })
+        .then(response => response.json())
+        .then(data => {
+            const nutritionFacts = document.getElementById("nutrition-facts")
+            nutritionFacts.innerHTML = ''
+            for (const key in data) {
+                const fact = document.createElement("p");
+                fact.innerText = `${key}: ${data[key]}`;
+                nutritionFacts.appendChild(fact);
+            }
+        })
+        .catch(error => console.error("Error: ", error));
+}
