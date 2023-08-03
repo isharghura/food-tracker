b1a772958bbb3888b572e18538f77a022fa29f67
isharghura <ishar.ghuras@gmail.com>
2023-08-03 19:05:31 -0400

    added comments

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const input = document.getElementById("user-input")
 const title = document.getElementById("title")
 
+
+//when a key is pressed
 document.addEventListener('keydown', (event) => {
     var isEnterKey = event.key;
     if (isEnterKey === 'Enter' && input.value !== '') {
