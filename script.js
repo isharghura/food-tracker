279ac7bd75f94cf52a3ac8fd91a1d73313bcfbdd
isharghura <ishar.ghuras@gmail.com>
2023-08-03 22:20:43 -0400

    reformatted code

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const input = document.getElementById("user-input");
-const title = document.getElementById("title");
-const factsList = document.getElementById("nutrition-facts");
-
-//fetch nutritional data of input
-async function getData(inputData) {
-    const config = await fetch("./config.json").then(response => response.json());
-    const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(config.apikey)}&query=${encodeURIComponent(inputData)}&dataType=${encodeURIComponent("Survey (FNDDS)")}&pageSize=${encodeURIComponent(1)}`;
-    const response = await fetch(api_url);
-    return response.json();
-}
-
 //when a key is pressed
 document.addEventListener('keydown', async (event) => {
+    const input = document.getElementById("user-input");
+    const title = document.getElementById("title");
+    const factsList = document.getElementById("nutrition-facts");
+
     var isEnterKey = event.key;
     if (isEnterKey === 'Enter' && input.value !== '') {
         //display nutrient facts
@@ -31,3 +23,11 @@ document.addEventListener('keydown', async (event) => {
         input.value = '';
     }
 });
+
+//fetch nutritional data of input
+async function getData(inputData) {
+    const config = await fetch("./config.json").then(response => response.json());
+    const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(config.apikey)}&query=${encodeURIComponent(inputData)}&dataType=${encodeURIComponent("Survey (FNDDS)")}&pageSize=${encodeURIComponent(1)}`;
+    const response = await fetch(api_url);
+    return response.json();
+}
