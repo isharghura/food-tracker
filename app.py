301dea33986f2f6f889db28a92a0d90589acd2b6
isharghura <ishar.ghuras@gmail.com>
2023-08-07 17:27:49 -0400

    added keepalive function

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-# from flask import Flask, render_template
-# from threading import Thread
+from flask import Flask, render_template
+from threading import Thread
 
-# app = Flask('')
+app = Flask('')
 
-# @app.route('/')
-# def home():
-#   return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html', static_folder = 'static')
   
-# def run():
-#     app.run(host='0.0.0.0', port=8080)
+def run():
+    app.run(host='0.0.0.0', port=8080)
   
-# def keep_alive():
-#   t = Thread(target=run)
-#   t.start()
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
-# keep_alive()
+keep_alive()
