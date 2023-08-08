93ad1633473874c37c14970a460858e98ec613eb
isharghura <ishar.ghuras@gmail.com>
2023-08-08 11:51:34 -0400

    added cors to flask file

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template
-from threading import Thread
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import requests
 
-app = Flask('')
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def home():
-    return render_template('index.html', static_folder = 'static')
+def index():
+    return render_template('index.html')
   
-def run():
-    app.run(host='0.0.0.0', port=8080)
-  
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
+if __name__ == '__main__':
+    app.run(debug=True)
