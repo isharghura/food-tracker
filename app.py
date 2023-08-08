2a5195ed502cb9c4f605b80bf193ef636762ac2c
isharghura <ishar.ghuras@gmail.com>
2023-08-08 12:00:43 -0400

    changed paths from relative to absolute

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 CORS(app)
 
 @app.route('/')
