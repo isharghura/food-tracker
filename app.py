0b057ae557ce0539238d3da1d1f8180864dba3bd
isharghura <ishar.ghuras@gmail.com>
2024-02-20 15:57:52 -0500

    added requirement

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def index():
     return render_template('index.html')
   
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
