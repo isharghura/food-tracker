3a3f56a657c6728ebb5991ddf83cbad1adb1ab4c
isharghura <ishar.ghuras@gmail.com>
2024-02-20 19:54:26 -0500

    added .env file

--- static/script.js
+++ static/script.js
@@ -1,13 +1,11 @@
-//when a key is pressed
-document.addEventListener('keydown', async (event) => {
-    const input = document.getElementById("user-input");
-    const title = document.getElementById("title");
-    const factsList = document.getElementById("nutrition-facts");
+const input = document.getElementById("user-input");
+const title = document.getElementById("title");
+const factsList = document.getElementById("nutrition-facts");
 
-    var isEnterKey = event.key;
-    if (isEnterKey === 'Enter' && input.value !== '') {
-        //display nutrient facts
-        const data = await getData(input.value);
+document.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter' && input.value !== '') {
+        const response = await fetch(`/api/getData/${encodeURIComponent(input.value)}`);
+        const data = await response.json();
         factsList.innerHTML = "";
         const nutrients = data.foods[0].foodNutrients;
         let output = "";
@@ -18,14 +16,5 @@ document.addEventListener('keydown', async (event) => {
         factsList.innerHTML = output;
         title.innerHTML = input.value + " (100g)";
         input.value = '';
-
     }
 });
-
-//fetch nutritional data of input
-async function getData(inputData) {
-    const config = await fetch("/static/config.json").then(response => response.json());
-    const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(config.apikey)}&query=${encodeURIComponent(inputData)}&dataType=${encodeURIComponent("Foundation")}&pageSize=${encodeURIComponent(1)}`;
-    const response = await fetch(api_url);
-    return response.json();
-}
---
+++ static/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+let app = express();
+const fetch = require('node-fetch');
+require('dotenv').config();
+const path = require('path');
+
+app.use('/static', express.static(path.resolve(__dirname, '../static')));
+
+app.get('/', async (req, res) => {
+    const indexPath = path.resolve(__dirname, '../templates/index.html');
+    res.sendFile(indexPath);
+})
+
+app.get('/api/getData/:inputData', async (req, res) => {
+    const inputData = req.params.inputData;
+    const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(process.env.apikey)}&query=${encodeURIComponent(inputData)}&dataType=${encodeURIComponent("Foundation")}&pageSize=${encodeURIComponent(1)}`;
+    try {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
